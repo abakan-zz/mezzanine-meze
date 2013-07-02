39f9bfe89f5dcfc68b811db8c75ddacace6d58fe
Ahmet Bakan <abakan@LinuxMint.(none)>
2013-07-01 22:15:26 -0400

    Changed package name, updated readme.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='mezzanine-meze',
+    name='Meze',
     version='0.1',
     packages=['meze'],
     license='BSD License',
